9e8bd5d9e4f515a282c346c239d5c7d975f9301d
onjoud <95690638+onjoud@users.noreply.github.com>
2022-04-23 11:58:38 +0200

    misajour

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ app.get("/",function(req,res){
     res.send("le serveur est en marche :) ")
 })
 
-app.listen(3000,()=>{
-    console.log("server started")
+app.get("/bonjour",function(req,res){
+    res.send("le serveur dit bonjour ")
 })
+
+app.use(express.urlencoded({extended:true}))
+app.listen(process.env.PORT || 5000,()=>{
+    console.log('now listening for requests on port',process.env.PORT);})
+
